42cb05d1173cc2291b5454b51a2f9dc7d27f0869
Alishah Safdari <alishahsafdari@gmail.com>
2023-06-01 08:10:38 +0430

    Fix linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,9 +3,10 @@ import Child from './CalculatorChild';
 
 const Component = () => (
   <div className="cal-contianer">
-    <div className="diplay">0</div>
+    <div className="diplay">
+      0
+    </div>
     <Child />
   </div>
 );
-
 export default Component;
