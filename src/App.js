5deb8bb9e7fce90b8cc213b1ad73b6934a79ae8f
Alishah Safdari <alishahsafdari@gmail.com>
2023-06-02 10:45:26 +0430

    Update the App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Component from './components/Calculator';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <Component />
+      <Quote />
     </div>
   );
 }
