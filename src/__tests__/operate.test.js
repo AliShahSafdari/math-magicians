0c524f6d080d685c3ec8eeb41fd22878e56fe662
Hiwot Bayissa <hiwina@gmail.com>
2023-06-08 11:24:40 +0300

    Test for division and multiplication

--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -4,4 +4,10 @@ describe('Check for the 4 main operation +, -, / and x', () => {
   test('1 + 2 should be 3', () => {
     expect(operate(1, 2, '+')).toEqual('3');
   });
+   test('5 - 2 should be 3', () => {
+    expect(operate(5, 2, '-')).toEqual('3');
+  });
+   test('1 * 2 should be 3', () => {
+    expect(operate(1, 2, '*')).toEqual('2');
+  });
 });
